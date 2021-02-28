54925ff1ef2e529712fa2e52f094f1983223e7a9
Yan Song <2470315257@qq.com>
2021-02-28 17:28:25 +0800

    计算器——控制台

---
+++ Homework1/Calculator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d1 = 0;
+            double d2 = 0;
+            Console.WriteLine("————计算器————");
+            Console.WriteLine("请输入第一个操作数：");
+            try
+            {
+                d1 = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("数字格式不正确");
+                return;
+            }
+
+            Console.WriteLine("请输入第二个操作数：");
+            try
+            {
+                d2 = double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("数字格式不正确");
+                return;
+            }
+
+            Console.WriteLine("请输入操作符：");
+            string s = Console.ReadLine();
+
+            double res = 0;
+
+            switch (s)
+            {
+                case "+":
+                    res = d1 + d2;
+                    break;
+                case "-":
+                    res = d1 - d2;
+                    break;
+                case "*":
+                    res = d1 * d2;
+                    break;
+                case "/":
+                    res = d1 / d2;
+                    break;
+                case "%":
+                    res = d1 % d2;
+                    break;
+                default:
+                    Console.WriteLine("您输入的符号无法识别");
+                    return;
+            }
+
+            Console.WriteLine("计算结果为：{0}", res);
+        }
+    }
+}
---
+++ Homework1/Calculator1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator1
+{
+    public partial class Form1 : Form
+    {
+        private double d1;
+        private double d2;
+        private string s;
+        private double res;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            s = textBox3.Text;
+
+            switch (s)
+            {
+                case "+":
+                    res = d1 + d2;
+                    break;
+                case "-":
+                    res = d1 - d2;
+                    break;
+                case "*":
+                    res = d1 * d2;
+                    break;
+                case "/":
+                    res = d1 / d2;
+                    break;
+                case "%":
+                    res = d1 % d2;
+                    break;
+                default:
+                    Console.WriteLine("您输入的符号无法识别");
+                    return;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("开始计算");
+            label6.Text = res.ToString();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            d1 = double.Parse(textBox1.Text);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            d2 = double.Parse(textBox2.Text);
+        }
+    }
+}
